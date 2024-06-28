740fe7a2f3dc1d45c60087aa20a81c47c4c9c4f5
unknown <sarawinget16@gmail.com>
2024-06-28 09:03:33 +0530

    commited

---
+++ myProject/src/Redux/Reducer.jsx
@@ -0,0 +1,20 @@
+const initialState = {
+    likes: 0,
+  };
+  const Reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return {
+          ...state,
+          likes: state.likes + 1,
+        };
+      case "DECREMENT":
+        return {
+          ...state,
+          likes:  state.likes - 1 ,
+        };
+      default:
+        return state;
+    }
+  };
+  export default Reducer;
